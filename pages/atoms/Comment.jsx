d56995113e244b9abdc3dc652761392c164c58a4
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-08-21 14:38:51 +0700

    update fixing undifined data

--- pages/atoms/Comment.jsx
+++ pages/atoms/Comment.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Comment = ({data}) => {
+const Comment = ({ data }) => {
   return (
-    <div className='flex gap-3'>
-        <img className='w-[3.5rem] h-[3.5rem] border-2 dark:border-cd300 border-cl300 rounded-full' src={data.user.image} alt="" />
-        <div className=''>
-            <h1 className='font-bold text-md'>{data.user.username}</h1>
-            <p className='font-thin text-sm text-cl300 dark:text-cd300'>{data.body}</p>
-        </div>
+    <div className="flex gap-3">
+      <img
+        className="w-[3.5rem] h-[3.5rem] border-2 dark:border-cd300 border-cl300 rounded-full"
+        src={data?.user?.image}
+        alt=""
+      />
+      <div className="">
+        <h1 className="font-bold text-md">{data?.user?.username}</h1>
+        <p className="font-thin text-sm text-cl300 dark:text-cd300">
+          {data?.body}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;
