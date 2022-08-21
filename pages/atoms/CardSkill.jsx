3e63e8a88ec7247725f14b65e2530ec4a1619263
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-08-21 14:49:09 +0700

    fix undified data

--- pages/atoms/CardSkill.jsx
+++ pages/atoms/CardSkill.jsx
@@ -8,9 +8,9 @@ const CardSkill = ({ data }) => {
       </div>
       <div className="">
         <h1 className=" flex text-sm md:text-base items-center gap-3 font-bold">
-          {data.name}
+          {data?.name}
         </h1>
-        <p className=" text-sm md:text-base font-thin">{data.level}</p>
+        <p className=" text-sm md:text-base font-thin">{data?.level}</p>
       </div>
     </div>
   );
